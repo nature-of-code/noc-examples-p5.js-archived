11012f44339cfc8a5e2026ff622b1e7ec1a3fbba
Pamela Fox <pamela.fox@gmail.com>
2014-02-04 14:37:37 -0800

    Update mover.js per Khan conventions
    It turns out I can't make comments on files, just commits, so I'm doing this as a commit, which I can undo if you'd like.
    The changes are that I declared the constructor using the var Bla = syntax, which is the only way we support declaring functions on Khan.
    I also added semi-colons to the end of the function declarations, as we also require those and they are best practice since those are indeed statements.

--- chp01_vectors/NOC_1_7_motion101/mover.js
+++ chp01_vectors/NOC_1_7_motion101/mover.js
@@ -1,24 +1,23 @@
 
-
 // The Nature of Code
 // Daniel Shiffman
 // http://natureofcode.com
 
-function Mover() {
+var Mover = function() {
   this.position = new PVector(random(width), random(height));
   this.velocity = new PVector(random(-2, 2), random(-2, 2));
-}
+};
 
 Mover.prototype.update = function() {
   this.position.add(this.velocity);
-}
+};
 
 Mover.prototype.display = function() {
   stroke(0);
   strokeWeight(2);
   fill(127);
   ellipse(this.position.x, this.position.y, 48, 48);
-}
+};
 
 Mover.prototype.checkEdges = function() {
 
@@ -35,6 +34,6 @@ Mover.prototype.checkEdges = function() {
   else if (this.position.y < 0) {
     this.position.y = height;
   }
-}
+};
 
 
