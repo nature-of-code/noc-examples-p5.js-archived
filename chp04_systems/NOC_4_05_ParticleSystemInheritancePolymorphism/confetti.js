945d1fa8a02273d0cf091866d543a60f086a8986
Daniel Shiffman <daniel@shiffman.net>
2014-02-22 13:10:38 -0500

    because i'm crazy about 2 spaces

--- chp04_systems/NOC_4_05_ParticleSystemInheritancePolymorphism/confetti.js
+++ chp04_systems/NOC_4_05_ParticleSystemInheritancePolymorphism/confetti.js
@@ -13,14 +13,14 @@ Confetti.prototype.constructor = Confetti;
 
 // Override the display method
 Confetti.prototype.display = function(){
-    rectMode(CENTER);
-    fill(127,this.lifespan);
-    stroke(0,this.lifespan);
-    strokeWeight(2);
-    pushMatrix();
-    translate(this.position.x,this.position.y);
-    var theta = map(this.position.x,0,width,0,TWO_PI*2);
-    rotate(theta);
-    rect(0,0,12,12);
-    popMatrix();
+  rectMode(CENTER);
+  fill(127,this.lifespan);
+  stroke(0,this.lifespan);
+  strokeWeight(2);
+  pushMatrix();
+  translate(this.position.x,this.position.y);
+  var theta = map(this.position.x,0,width,0,TWO_PI*2);
+  rotate(theta);
+  rect(0,0,12,12);
+  popMatrix();
 }
