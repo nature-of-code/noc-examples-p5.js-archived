e458664eb504b71db8860f3aa987073d02972681
Daniel Shiffman <daniel@shiffman.net>
2014-02-28 22:16:52 -0500

    refactor 5.2

--- chp05_libraries/box2dweb/NOC_5_02_Boxes/sketch.js
+++ chp05_libraries/box2dweb/NOC_5_02_Boxes/sketch.js
@@ -2,24 +2,6 @@
 // Daniel Shiffman
 // http://natureofcode.com
 
-// Making it easier to use all these classes
-var Vec2 = Box2D.Common.Math.b2Vec2;
-var BodyDef = Box2D.Dynamics.b2BodyDef;
-var Body = Box2D.Dynamics.b2Body;
-var FixtureDef = Box2D.Dynamics.b2FixtureDef;
-var Fixture = Box2D.Dynamics.b2Fixture;
-var World = Box2D.Dynamics.b2World;
-var MassData = Box2D.Collision.Shapes.b2MassData;
-var PolygonShape = Box2D.Collision.Shapes.b2PolygonShape;
-var CircleShape = Box2D.Collision.Shapes.b2CircleShape;
-var DebugDraw = Box2D.Dynamics.b2DebugDraw;
-var Transform = Box2D.Common.Math.b2Transform;
-
-// variables for transformations
-var transX;
-var transY;
-var scaleFactor;
-var yFlip;
 
 // A reference to our box2d world
 var world;
@@ -32,15 +14,8 @@ var boxes = [];
 function setup() {
   createGraphics(640,360);
 
-  transX = width/2;
-  transY = height/2;
-  scaleFactor = 10;
-  yFlip = -1;
-
   // Initialize box2d physics and create the world
-  // This part was always taken care of by PBox2D
-  // Arguments are gravity and sleeping
-  world = new World(new Vec2(0, 10),true);
+  world = createWorld();
 
   // Add a bunch of fixed boundaries
   boundaries.push(new Boundary(width/4,height-5,width/2-50,10));
