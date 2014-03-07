8057ac869da117a78bae56c34c8e92f2b832d7c9
loosenutsInc <binuabraham990@gmail.com>
2014-03-07 12:50:54 +0530

    Updation

---
+++ chp09_ga/NOC_9_01_GA_Shakespeare/DNA.js
@@ -0,0 +1,40 @@
+function DNA(num) {
+  	this.genes = new Array(num);
+  	this.fitness;
+  	for (var i = 0; i < this.genes.length ; i++) {
+      		this.genes[i] = String.fromCharCode(random(32,128));
+    	}
+}
+  
+DNA.prototype.getPhrase = function() {
+	return this.genes.join("");
+}
+  
+DNA.prototype.fitness =function(target) {
+     	var score = 0;
+     	for (var i = 0; i < this.genes.length; i++) {
+        	if (this.genes[i] == target.charAt(i)) {
+          		score++;
+        	}
+	}
+          
+     	this.fitness = parseFloat(score) / parseFloat(target.length);
+}
+  
+DNA.prototype.crossover = function(partner) {
+    	var child = new DNA(this.genes.length);
+    	var midpoint = Math.floor(random(this.genes.length));
+	for (var i = 0; i < this.genes.length; i++) {
+      		if (i > midpoint) child.genes[i] = this.genes[i];
+      		else              child.genes[i] = partner.genes[i];
+    	}
+    	return child;
+}
+
+DNA.prototype.mutate = function(mutationRate) {
+    	for (var i = 0; i < this.genes.length; i++) {
+      		if (random(1) < mutationRate) {
+        		this.genes[i] = String.fromCharCode(random(32,128));
+      		}
+    	}
+}
