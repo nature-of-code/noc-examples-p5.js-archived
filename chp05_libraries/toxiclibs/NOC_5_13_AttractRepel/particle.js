1c64c323545686827ba529dc83a29f475af06611
Daniel Shiffman <daniel@shiffman.net>
2014-03-07 22:08:24 -0500

    5.13 attract and repel

---
+++ chp05_libraries/toxiclibs/NOC_5_13_AttractRepel/particle.js
@@ -0,0 +1,23 @@
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+
+// Child class constructor
+function Particle(position, radius, range, strength) {
+  VerletParticle2D.call(this,position);
+  this.r = radius;
+  physics.addParticle(this);
+  physics.addBehavior(new AttractionBehavior(this, range, strength));
+}
+
+// Inherit from the parent class
+Particle.prototype = Object.create(VerletParticle2D.prototype);
+Particle.prototype.constructor = Particle;
+
+// Override the display method
+Particle.prototype.display = function(){
+  fill(127);
+  stroke(200);
+  strokeWeight(2);
+  ellipse(this.x,this.y,this.r*2,this.r*2);
+}
