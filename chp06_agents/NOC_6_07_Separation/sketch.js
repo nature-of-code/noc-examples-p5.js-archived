56c03079632be5531cb41c88a680b3c2fd4fcfca
Daniel Shiffman <daniel@shiffman.net>
2014-03-10 16:21:46 -0400

    6.7 separation

---
+++ chp06_agents/NOC_6_07_Separation/sketch.js
@@ -0,0 +1,43 @@
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+
+// Separation
+// Via Reynolds: http://www.red3d.com/cwr/steer/
+
+// A list of vehicles
+var vehicles = [];
+
+function setup() {
+  createGraphics(640,360);
+  // We are now making random vehicles and storing them in an array
+  for (var i = 0; i < 100; i++) {
+    vehicles.push(new Vehicle(random(width),random(height)));
+  }
+}
+
+function draw() {
+  background(51);
+
+  for (var i = 0; i < vehicles.length; i++) {
+    // Path following and separation are worked on in this function
+    vehicles[i].separate(vehicles);
+    // Call the generic run method (update, borders, display, etc.)
+    vehicles[i].update();
+    vehicles[i].borders();
+    vehicles[i].display();
+  }
+
+  // Instructions
+  fill(200);
+  noStroke();
+  text("Drag the mouse to generate new vehicles.",10,height-16);
+}
+
+
+function mouseDragged() {
+  vehicles.push(new Vehicle(mouseX,mouseY));
+}
+
+
+
