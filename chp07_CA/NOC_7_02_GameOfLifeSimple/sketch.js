afe73f1203e59e59d4072e24bdfa6071067aef34
Daniel Shiffman <daniel@shiffman.net>
2014-03-30 22:42:44 -0400

    more cleanup and comments #18

--- chp07_CA/NOC_7_02_GameOfLifeSimple/sketch.js
+++ chp07_CA/NOC_7_02_GameOfLifeSimple/sketch.js
@@ -1,18 +1,28 @@
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+
+// A basic implementation of John Conway's Game of Life CA
+// how could this be improved to use object oriented programming?
+// think of it as similar to our particle system, with a "cell" class
+// to describe each individual cell and a "cellular automata" class
+// to describe a collection of cells
+
 var gol;
 
 function setup() {
-  	createCanvas(640, 360);
-  	setFrameRate(24);
-  	gol = new GOL();
+    createCanvas(640, 360);
+    gol = new GOL();
 }
 
 function draw() {
-  	background(255);
-  	gol.generate();
-  	gol.display();
+    background(255);
+    gol.generate();
+    gol.display();
 };
 
+// reset board when mouse is pressed
 function mousePressed() {
-  	gol.init();
+    gol.init();
 };
 
