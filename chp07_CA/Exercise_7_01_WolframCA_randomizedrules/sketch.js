1e9d87dff669d20849fd4fcc863f1f77344da62e
Daniel Shiffman <daniel@shiffman.net>
2014-03-30 22:27:44 -0400

    playing around with indentation

--- chp07_CA/Exercise_7_01_WolframCA_randomizedrules/sketch.js
+++ chp07_CA/Exercise_7_01_WolframCA_randomizedrules/sketch.js
@@ -3,30 +3,30 @@ var ca;
 var delay = 0;
 
 function setup() {
-  	createCanvas(640, 360);
-  	background(51);
-  	var ruleset = new Array(0, 1, 0, 1, 1, 0, 1, 0);
-  	ca = new CA(ruleset);
+    createCanvas(640, 360);
+    background(51);
+    var ruleset = new Array(0, 1, 0, 1, 1, 0, 1, 0);
+    ca = new CA(ruleset);
 }
 
 function draw() {
-  	ca.display();
-  	ca.generate();
+    ca.display();
+    ca.generate();
 
-  	if (ca.finished()) {
-    		delay++;
-    		if (delay > 30) {
-      			background(51);
-      			ca.randomize();
-      			ca.restart();
-      			delay = 0;
-    		}
-  	}
+    if (ca.finished()) {
+        delay++;
+        if (delay > 30) {
+            background(51);
+            ca.randomize();
+            ca.restart();
+            delay = 0;
+        }
+    }
 };
 
 function mousePressed() {
-  	background(255);
-  	ca.randomize();
-  	ca.restart();
+    background(255);
+    ca.randomize();
+    ca.restart();
 };
 
--- chp08_fractals/NOC_8_01_Recursion/sketch.js
+++ chp08_fractals/NOC_8_01_Recursion/sketch.js
@@ -1,17 +1,27 @@
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+
+// Simple Recursion
+
 function setup() {
-	createCanvas(640,360);
+  createCanvas(640,360);
 }
 
 function draw() {
-	background(51);
-	drawCircle(width/2, height/2, width);
-	noLoop();
-};
+  background(51);
+  drawCircle(width/2, height/2, width);
+  noLoop();
+}
 
+// Very simple function that draws one circle 
+// and recursively calls itself
 function drawCircle(x, y, r) {
-	ellipse(x, y, r, r);
-	if(r>2) {
-		r*=0.75;
-		drawCircle(x, y, r, r);
-	}
-};
+  ellipse(x, y, r, r);
+   // Exit condition, stop when radius is too small
+  if(r>2) {
+    r*=0.75;
+    // Call the function inside the function! (recursion!)
+    drawCircle(x, y, r, r);
+  }
+}
