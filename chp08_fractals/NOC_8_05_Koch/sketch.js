97611e267254fd1e5941d803d437da93cb3ef165
Daniel Shiffman <daniel@shiffman.net>
2014-04-09 15:59:22 -0400

    fixing up chapter 8, formatting and comments

--- chp08_fractals/NOC_8_05_Koch/sketch.js
+++ chp08_fractals/NOC_8_05_Koch/sketch.js
@@ -1,17 +1,28 @@
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+// Koch Curve
+
+// Renders a simple fractal, the Koch snowflake
+// Each recursive level drawn in sequence
+
 var k;
 
 function setup() {
-	createCanvas(800,250);
-	setFrameRate(1);
-	k = new KochFractal();
-	smooth();
+  createCanvas(640,360);
+  background(255);
+  frameRate(1);  // Animate slowly
+  k = new KochFractal();
 }
 
 function draw() {
-	background(255);
-	k.render();
-  	k.nextLevel();
-  	if (k.getCount() > 5) {
-    		k.restart();
-  	}
-};
+  background(51);
+  // Draws the snowflake!
+  k.render();
+  // Iterate
+  k.nextLevel();
+  // Let's not do it more than 5 times. . .
+  if (k.getCount() > 5) {
+    k.restart();
+  }
+}
