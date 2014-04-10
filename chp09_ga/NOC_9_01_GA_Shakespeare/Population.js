aa5e614f5ac6955fe214f4cf1b5b51db541edda6
Daniel Shiffman <daniel@shiffman.net>
2014-04-09 22:29:45 -0400

    GA shakespear 9.1 example

--- chp09_ga/NOC_9_01_GA_Shakespeare/Population.js
+++ chp09_ga/NOC_9_01_GA_Shakespeare/Population.js
@@ -1,89 +1,119 @@
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+
+// Genetic Algorithm, Evolving Shakespeare
+
+// A class to describe a population of virtual organisms
+// In this case, each organism is just an instance of a DNA object
+
 function Population(p, m, num) {
-	this.mutationRate = m;
-  	this.population = new Array(num);
-  	this.matingPool = new Array();
-  	this.target = p;
-  	this.generations = 0;
-  	this.finished = false;
-  	this.perfectScore = 1;
-	for (var i = 0; i < this.population.length; i++) {
-      		this.population[i] = new DNA(this.target.length);
-    	}
-    	this.calcFitness();
+
+  this.population;                   // Array to hold the current population
+  this.matingPool;                   // ArrayList which we will use for our "mating pool"
+  this.generations = 0;              // Number of generations
+  this.finished = false;             // Are we finished evolving?
+  this.target = p;                   // Target phrase
+  this.mutationRate = m;             // Mutation rate
+  this.perfectScore = 1;
+
+  this.population = [];
+  for (var i = 0; i < num; i++) {
+    this.population[i] = new DNA(this.target.length);
+  }
+  this.calcFitness();
+  this.matingPool = [];
 }
 
+// Fill our fitness array with a value for every member of the population
 Population.prototype.calcFitness = function() {
-    	for (var i = 0; i < this.population.length; i++) {
-      		this.population[i].fitness(this.target);
-    	}
+  for (var i = 0; i < this.population.length; i++) {
+    this.population[i].calcFitness(target);
+  }
 }
 
+// Generate a mating pool
 Population.prototype.naturalSelection = function() {
-	this.matingPool.length = 0;
-    	var maxFitness = 0;
-    	for (var i = 0; i < this.population.length; i++) {
-      		if (this.population[i].fitness > maxFitness) {
-        		maxFitness = this.population[i].fitness;
-      		}
-    	}
-    	for (var i = 0; i < this.population.length; i++) {
-		var fitness = map(this.population[i].fitness,0,maxFitness,0,1);
-		var n = Math.floor(fitness * 100); 
-      		for (var j = 0; j < n; j++) {
-        		this.matingPool.push(this.population[i]);
-      		}
-    	}
-}
+  // Clear the ArrayList
+  this.matingPool = [];
 
-Population.prototype.generate = function() {
-	for (var i = 0; i < this.population.length; i++) {
-      		var a = Math.floor(random(this.matingPool.length));
-      		var b = Math.floor(random(this.matingPool.length));
-      		var partnerA = this.matingPool[a];
-		var partnerB = this.matingPool[b];
-      		var child = partnerA.crossover(partnerB);
-      		child.mutate(this.mutationRate);
-      		this.population[i] = child;
-    	}
-    	this.generations++;
-}
+  var maxFitness = 0;
+  for (var i = 0; i < this.population.length; i++) {
+    if (this.population[i].fitness > maxFitness) {
+      maxFitness = this.population[i].fitness;
+    }
+  }
 
-Population.prototype.getBest = function() {
-    	var worldrecord = 0;
-    	var index = 0;
-    	for (var i = 0; i < this.population.length; i++) {
-      		if (this.population[i].fitness > worldrecord) {
-        		index = i;
-        		worldrecord = this.population[i].fitness;
-      		}
-    	}
+  // Based on fitness, each member will get added to the mating pool a certain number of times
+  // a higher fitness = more entries to mating pool = more likely to be picked as a parent
+  // a lower fitness = fewer entries to mating pool = less likely to be picked as a parent
+  for (var i = 0; i < this.population.length; i++) {
     
-    	if (worldrecord == this.perfectScore ) this.finished = true;
-    	return this.population[index].getPhrase();
+    var fitness = map(this.population[i].fitness,0,maxFitness,0,1);
+    var n = floor(fitness * 100);  // Arbitrary multiplier, we can also use monte carlo method
+    for (var j = 0; j < n; j++) {              // and pick two random numbers
+      this.matingPool.push(this.population[i]);
+    }
+  }
 }
 
-Population.prototype.finished = function() {
-    	return this.finished;
+// Create a new generation
+Population.prototype.generate = function() {
+  // Refill the population with children from the mating pool
+  for (var i = 0; i < this.population.length; i++) {
+    var a = floor(random(this.matingPool.length));
+    var b = floor(random(this.matingPool.length));
+    var partnerA = this.matingPool[a];
+    var partnerB = this.matingPool[b];
+    var child = partnerA.crossover(partnerB);
+    child.mutate(this.mutationRate);
+    this.population[i] = child;
+  }
+  this.generations++;
+}
+
+
+// Compute the current "most fit" member of the population
+Population.prototype.getBest = function() {
+  var worldrecord = 0.0;
+  var index = 0;
+  for (var i = 0; i < this.population.length; i++) {
+    if (this.population[i].fitness > worldrecord) {
+      index = i;
+      worldrecord = this.population[i].fitness;
+    }
+  }
+  
+  if (worldrecord == this.perfectScore) this.finished = true;
+  return this.population[index].getPhrase();
+}
+
+Population.prototype.isFinished = function() {
+  return this.finished;
 }
 
 Population.prototype.getGenerations = function() {
-    	return this.generations;
+  return this.generations;
 }
 
+// Compute average fitness for the population
 Population.prototype.getAverageFitness = function() {
-    	var total = 0;
-    	for (var i = 0; i < this.population.length; i++) {
-      		total += this.population[i].fitness;
-    	}
-    	return total / (this.population.length);
+  var total = 0;
+  for (var i = 0; i < this.population.length; i++) {
+    total += this.population[i].fitness;
+  }
+  return total / (this.population.length);
 }
 
 Population.prototype.allPhrases = function() {
-    	var everything = "";
-    
-    	var displayLimit = min(this.population.length,50);
-    	for (var i = 0; i < displayLimit; i++) {
-      		everything += this.population[i].getPhrase() + "\n";
-    	}
-    	return everything;
+  var everything = "";
+  
+  var displayLimit = min(this.population.length,50);
+  
+  
+  for (var i = 0; i < displayLimit; i++) {
+    everything += this.population[i].getPhrase() + "<br>";
+  }
+  return everything;
 }
+
--- chp09_ga/NOC_9_01_GA_Shakespeare/sketch.js
+++ chp09_ga/NOC_9_01_GA_Shakespeare/sketch.js
@@ -1,48 +1,95 @@
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+
+// Genetic Algorithm, Evolving Shakespeare
+
+// Demonstration of using a genetic algorithm to perform a search
+
+// setup()
+//  # Step 1: The Population 
+//    # Create an empty population (an array or ArrayList)
+//    # Fill it with DNA encoded objects (pick random values to start)
+
+// draw()
+//  # Step 1: Selection 
+//    # Create an empty mating pool (an empty ArrayList)
+//    # For every member of the population, evaluate its fitness based on some criteria / function, 
+//      and add it to the mating pool in a manner consistant with its fitness, i.e. the more fit it 
+//      is the more times it appears in the mating pool, in order to be more likely picked for reproduction.
+
+//  # Step 2: Reproduction Create a new empty population
+//    # Fill the new population by executing the following steps:
+//       1. Pick two "parent" objects from the mating pool.
+//       2. Crossover -- create a "child" object by mating these two parents.
+//       3. Mutation -- mutate the child's DNA based on a given probability.
+//       4. Add the child object to the new population.
+//    # Replace the old population with the new population
+//  
+//   # Rinse and repeat
+
+
 var target;
 var popmax;
 var mutationRate;
 var population;
 
-function setup() {
-	createCanvas(1000, 360);
-  	target = "To be or not to be.";
-  	popmax = 150;
-  	mutationRate = 0.01;
+var bestPhrase;
+var allPhrases;
+var stats;
 
-  	population = new Population(target, mutationRate, popmax);
+function setup() {
+  bestPhrase = createHTML("Best phrase:");
+  bestPhrase.position(10,10);
+  bestPhrase.class("best");
+
+  allPhrases = createHTML("All phrases:");
+  allPhrases.position(600,10);
+  allPhrases.class("all");
+
+  stats = createHTML("Stats");
+  stats.position(10,200);
+  stats.class("stats");
+  
+  //createCanvas(640, 360);
+  target = "To be or not to be.";
+  popmax = 150;
+  mutationRate = 0.01;
+
+  // Create a populationation with a target phrase, mutation rate, and populationation max
+  population = new Population(target, mutationRate, popmax);
 }
 
 function draw() {
-  	population.naturalSelection();
-  	population.generate();
-  	population.calcFitness();
-  	displayInfo();
+  // Generate mating pool
+  population.naturalSelection();
+  //Create next generation
+  population.generate();
+  // Calculate fitness
+  population.calcFitness();
+  displayInfo();
 
-  	if (population.finished == true) {
-    		println(millis()/1000.0);
-    		noLoop();
-  	}
-};
+  // If we found the target phrase, stop
+  if (population.isFinished()) {
+    println(millis()/1000.0);
+    noLoop();
+  }
+}
 
 function displayInfo() {
-  	background(255);
-	var answer = population.getBest();
-  	textAlign(LEFT);
-  	fill(0);
+  // Display current status of populationation
+  var answer = population.getBest();
   
+  bestPhrase.html("Best phrase:<br>" + answer);
   
-  	textSize(24);
-  	text("Best phrase:",20,30);
-  	textSize(40);
-  	text(answer, 20, 100);
-	
-  	textSize(18);
-  	text("total generations:     " + population.getGenerations(), 20, 160);
-  	text("average fitness:       " + nf(population.getAverageFitness(), 0, 2), 20, 180);
-  	text("total population: " + popmax, 20, 200);
-  	text("mutation rate:         " + Math.floor(mutationRate * 100) + "%", 20, 220);
- 
-	textFont("Courier");
-  	textSize(10);
-  	text("All phrases:" + population.allPhrases() + "\n", 500, 10);
-};
+  var statstext = "total generations:     " + population.getGenerations() + "<br>";
+  statstext +=    "average fitness:       " + nf(population.getAverageFitness()) + "<br>";
+  statstext +=    "total population:      " + popmax + "<br>";
+  statstext +=    "mutation rate:         " + floor(mutationRate * 100) + "%";
+  
+  stats.html(statstext);
+
+  allPhrases.html("All phrases:<br>" + population.allPhrases())
+}
+
+
