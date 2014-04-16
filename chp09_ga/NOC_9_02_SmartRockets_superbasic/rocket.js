4bf7b9540c7eab62521542873dec7f2b7a307801
Daniel Shiffman <daniel@shiffman.net>
2014-04-16 12:18:33 -0400

    smart rockets

---
+++ chp09_ga/NOC_9_02_SmartRockets_superbasic/rocket.js
@@ -0,0 +1,98 @@
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+
+// Pathfinding w/ Genetic Algorithms
+
+// Rocket class -- this is just like our Boid / Particle class
+// the only difference is that it has DNA & fitness
+
+//constructor
+function Rocket(l, dna_) {
+  // All of our physics stuff
+  this.acceleration = new PVector();
+  this.velocity = new PVector();
+  this.location = l.get();
+  // Size
+  this.r = 4;
+  // Fitness and DNA
+  this.fitness = 0;
+  this.dna = dna_;
+  // To count which force we're on in the genes
+  this.geneCounter = 0;
+
+  this.hitTarget = false;   // Did I reach the target
+}
+
+// Fitness function
+// fitness = one divided by distance squared
+Rocket.prototype.fitness = function() {
+  var d = dist(location.x, location.y, target.x, target.y);
+  this.fitness = pow(1/d, 2);
+};
+
+// Run in relation to all the obstacles
+// If I'm stuck, don't bother updating or checking for intersection
+Rocket.prototype.run = function() {
+  this.checkTarget(); // Check to see if we've reached the target
+  if (!this.hitTarget) {
+    this.applyForce(this.dna.genes[this.geneCounter]);
+    this.geneCounter = (this.geneCounter + 1) % this.dna.genes.length;
+    this.update();
+  }
+  this.display();
+};
+
+// Did I make it to the target?
+Rocket.prototype.checkTarget = function() {
+  var d = dist(this.location.x, this.location.y, target.x, target.y);
+  if (d < 12) {
+    this.hitTarget = true;
+  }
+};
+
+Rocket.prototype.applyForce = function(f) {
+  this.acceleration.add(f);
+};
+
+Rocket.prototype.update = function() {
+  this.velocity.add(this.acceleration);
+  this.location.add(this.velocity);
+  this.acceleration.mult(0);
+};
+
+Rocket.prototype.display = function() {
+  // TODO: Fix to work with radians https://github.com/lmccart/p5.js/pull/185#issuecomment-40615973
+  angleMode(DEGREES);
+  var theta = degrees(this.velocity.heading()) + 90;
+  var r = this.r;
+  fill(200, 100);
+  stroke(0);
+  pushMatrix();
+  translate(this.location.x, this.location.y);
+  rotate(theta);
+
+  // Thrusters
+  rectMode(CENTER);
+  fill(0);
+  rect(-r/2, r*2, r/2, r);
+  rect(r/2, r*2, r/2, r);
+
+  // Rocket body
+  fill(175);
+  beginShape(TRIANGLES);
+  vertex(0, -r*2);
+  vertex(-r, r*2);
+  vertex(r, r*2);
+  endShape(CLOSE);
+
+  popMatrix();
+};
+
+Rocket.prototype.getFitness = function() {
+  return this.fitness;
+};
+
+Rocket.prototype.getDNA = function() {
+  return this.dna;
+};
---
+++ chp09_ga/NOC_9_02_SmartRockets_superbasic/sketch.js
@@ -0,0 +1,73 @@
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+
+// Smart Rockets w/ Genetic Algorithms
+
+// Each Rocket's DNA is an array of PVectors
+// Each PVector acts as a force for each frame of animation
+// Imagine an booster on the end of the rocket that can point in any direction
+// and fire at any strength every frame
+
+// The Rocket's fitness is a function of how close it gets to the target as well as how fast it gets there
+
+// This example is inspired by Jer Thorp's Smart Rockets
+// http://www.blprnt.com/smartrockets/
+
+var lifetime;  // How long should each generation live
+
+var population;  // Population
+
+var lifeCounter;   // Timer for cycle of generation
+
+var target;        // Target location
+
+function setup() {
+  createCanvas(640, 360);
+  // The number of cycles we will allow a generation to live
+  lifetime = height;
+
+  // Initialize variables
+  lifeCounter = 0;
+  
+  target = new PVector(width/2, 24);
+
+  // Create a population with a mutation rate, and population max
+  var mutationRate = 0.01;
+  population = new Population(mutationRate, 50);
+
+}
+
+function draw() {
+  background(255);
+
+  // Draw the start and target locations
+  fill(0);
+  ellipse(target.x,target.y,24,24);
+
+
+  // If the generation hasn't ended yet
+  if (lifeCounter < lifetime) {
+    population.live();
+    lifeCounter++;
+    // Otherwise a new generation
+  }
+  else {
+    lifeCounter = 0;
+    population.fitness();
+    population.selection();
+    population.reproduction();
+  }
+
+  // Display some info
+  fill(0);
+  text("Generation #: " + population.getGenerations(), 10, 18);
+  text("Cycles left: " + (lifetime-lifeCounter), 10, 36);
+}
+
+// Move the target if the mouse is pressed
+// System will adapt to new target
+function mousePressed() {
+  target.x = mouseX;
+  target.y = mouseY;
+}
