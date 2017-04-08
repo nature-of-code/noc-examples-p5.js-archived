d4af94f8a637017ab73263b2f9c1883cc9678475
Daniel Shiffman <daniel@shiffman.net>
2017-04-08 10:21:36 -0400

    fixed up perceptron port

--- chp10_nn/NOC_10_01_Perceptron/perceptron.js
+++ chp10_nn/NOC_10_01_Perceptron/perceptron.js
@@ -1,56 +1,56 @@
+// Daniel Shiffman
 // The Nature of Code
 // http://natureofcode.com
 
-// Neural Network: Perceptron
+// Simple Perceptron Example
+// See: http://en.wikipedia.org/wiki/Perceptron
 
-// A Simple Perceptron Class
-// Includes functionality for user to train network with known data,
-// and generate an output based on an array of inputs
+// Perceptron Class
 
+// Perceptron is created with n weights and learning constant
+function Perceptron(n, c) {
+  // Array of weights for inputs
+  this.weights = new Array(n);
+  // Start with random weights
+  for (var i = 0; i < this.weights.length; i++) {
+    this.weights[i] = random(-1,1);
+  }
+  this.c = c; // learning rate/constant
+}
 
-function Perceptron(num_of_inputs) {
-	this.weights = [];
-	this.c = 0.01; // learning constant
-	
+// Function to train the Perceptron
+// Weights are adjusted based on "desired" answer
+Perceptron.prototype.train = function(inputs, desired) {
+  // Guess the result
+  var guess = this.feedforward(inputs);
+  // Compute the factor for changing the weight based on the error
+  // Error = desired output - guessed output
+  // Note this can only be 0, -2, or 2
+  // Multiply by learning constant
+  var error = desired - guess;
+  // Adjust weights based on weightChange * input
+  for (var i = 0; i < this.weights.length; i++) {
+    this.weights[i] += this.c * error * inputs[i];
+  }
+}
 
-	// Weights start off random 
-	for (var i = 0; i < num_of_inputs; i++) {
-		this.weights[i] = Math.random(-1,1);
-	}
+// Guess -1 or 1 based on input values
+Perceptron.prototype.feedforward = function(inputs) {
+  // Sum all values
+  var sum = 0;
+  for (var i = 0; i < this.weights.length; i++) {
+    sum += inputs[i] * this.weights[i];
+  }
+  // Result is sign of the sum, -1 or 1
+  return this.activate(sum);
+}
 
+Perceptron.prototype.activate = function(sum) {
+  if (sum > 0) return 1;
+  else return -1;
+}
 
-	// Return an output based on inputs
-	this.feedforward = function(inputs) {
-		var sum = 0;
-		for (var i = 0; i < this.weights.length; i++) {
-			sum += inputs[i] * this.weights[i];
-			console.log(i+": "+inputs[i]+", "+this.weights[i]);
-		}
-		console.log("sum: "+sum);
-		return this.activate(sum);
-	}
-
-
-	// Output is +1 or -1
-	this.activate = function(sum) {
-		if (sum > 0) return 1;
-		else return -1;
-	}
-
-
-	// Train network against known data
-	this.train = function(inputs, desired) {
-		var guess = this.feedforward(inputs);
-		var error = desired - guess;
-
-		for (var i = 0; i < this.weights.length; i++) {
-			this.weights[i] += this.c * error * inputs[i];
-		}
-
-	}
-
-} 
-
-
-
-
+// Return weights
+Perceptron.prototype.getWeights = function() {
+  return this.weights;
+}
--- chp10_nn/NOC_10_01_Perceptron/sketch.js
+++ chp10_nn/NOC_10_01_Perceptron/sketch.js
@@ -1,69 +1,97 @@
 // The Nature of Code
+// Daniel Shiffman
 // http://natureofcode.com
 
-// Neural Network: Perceptron
+// Simple Perceptron Example
+// See: http://en.wikipedia.org/wiki/Perceptron
 
-// Train a simple perceptron
-// Draws a line, and train the perceptron to guess whether a random point 
-// is above or below a line. 
+// Code based on text "Artificial Intelligence", George Luger
 
+// A list of points we will use to "train" the perceptron
+var training = new Array(2000);
+// A Perceptron object
 var ptron;
-var training = [];
+
+// We will train the perceptron with one "Point" object at a time
 var count = 0;
 
-// calculate line
-function f(x) {
-	return 2 * x + 1;
-}
+// Coordinate space
+var xmin = -1;
+var ymin = -1;
+var xmax = 1;
+var ymax = 1;
 
-function Trainer(x, y, a) {	
-	this.inputs = [x, y, 1]; // 1 is bias
-	this.answer = a;
+// The function to describe a line
+function f(x) {
+  var y = 0.3 * x + 0.4;
+  return y;
 }
 
 function setup() {
-  createCanvas(window.innerWidth,window.innerHeight);
-  
-  ptron = new Perceptron(3);
+  createCanvas(400, 400);
 
-  for (var i = 0; i < 2000; i++) {
-  	var x = random(-width/2, width/2);
-  	var y = random(-height/2, height/2);
+  // The perceptron has 3 inputs -- x, y, and bias
+  // Second value is "Learning Constant"
+  ptron = new Perceptron(3, 0.001); // Learning Constant is low just b/c it's fun to watch, this is not necessarily optimal
 
-  	var answer = 1;
-  	if (y < f(x)) answer = -1;
-
-  	training[i] = new Trainer(x, y, answer);
+  // Create a random set of training points and calculate the "known" answer
+  for (var i = 0; i < training.length; i++) {
+    var x = random(xmin, xmax);
+    var y = random(ymin, ymax);
+    var answer = 1;
+    if (y < f(x)) answer = -1;
+    training[i] = {
+      input: [x, y, 1],
+      output: answer
+    };
   }
-  
-};
+}
+
 
 function draw() {
-  background(255);
-  translate(width/2, height/2);
+  background(0);
 
-  // draw line
-  stroke(126);
-  line(-width/2, f(-width/2), width/2, f(width/2));
+  // Draw the line
+  strokeWeight(1);
+  stroke(255);
+  var x1 = map(xmin, xmin, xmax, 0, width);
+  var y1 = map(f(xmin), ymin, ymax, height, 0);
+  var x2 = map(xmax, xmin, xmax, 0, width);
+  var y2 = map(f(xmax), ymin, ymax, height, 0);
+  line(x1, y1, x2, y2);
+
+  // Draw the line based on the current weights
+  // Formula is weights[0]*x + weights[1]*y + weights[2] = 0
+  stroke(255);
+  strokeWeight(2);
+  var weights = ptron.getWeights();
+  var x1 = xmin;
+  var y1 = (-weights[2] - weights[0] * x1) / weights[1];
+  var x2 = xmax;
+  var y2 = (-weights[2] - weights[0] * x2) / weights[1];
+
+  var x1 = map(x1, xmin, xmax, 0, width);
+  var y1 = map(y1, ymin, ymax, height, 0);
+  var x2 = map(x2, xmin, xmax, 0, width);
+  var y2 = map(y2, ymin, ymax, height, 0);
+  line(x1, y1, x2, y2);
 
 
-  // train one point at a time
-  ptron.train(training[count].inputs, training[count].answer);
+  // Train the Perceptron with one "training" point at a time
+  ptron.train(training[count].input, training[count].output);
   count = (count + 1) % training.length;
-  
+
+  // Draw all the points based on what the Perceptron would "guess"
+  // Does not use the "known" correct answer
   for (var i = 0; i < count; i++) {
-  	stroke(0);
-  	var guess = ptron.feedforward(training[i].inputs);
-  	if (guess > 0) noFill();
-  	else 		   fill(0);
-  	ellipse(training[i].inputs[0], training[i].inputs[1], 8, 8);
-  }  
+    stroke(255);
+    strokeWeight(1);
+    fill(255);
+    var guess = ptron.feedforward(training[i].input);
+    if (guess > 0) noFill();
 
-};
-
-
-
-
-
-
-
+    var x = map(training[i].input[0], xmin, xmax, 0, width);
+    var y = map(training[i].input[1], ymin, ymax, height, 0);
+    ellipse(x, y, 8, 8);
+  }
+}
