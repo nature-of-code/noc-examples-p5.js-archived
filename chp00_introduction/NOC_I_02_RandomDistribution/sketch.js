560a0a47e0575a74f8769b5922e56010644da5c8
VRamazing <r.vignesh149@gmail.com>
2017-10-06 14:33:54 +0530

    NOC_I_02_ Random Distribution:Updated p5 js link,Added comments to code,added title to page,Replaced arr.length with length in code

--- chp00_introduction/NOC_I_02_RandomDistribution/sketch.js
+++ chp00_introduction/NOC_I_02_RandomDistribution/sketch.js
@@ -6,28 +6,29 @@
 // An array to keep track of how often random numbers are picked
 
 var randomCounts = [];
-var total = 20;
+var total = 20; //Number random  numbers we want to track
 
 function setup() {
   createCanvas(640,360);
   for (var i = 0; i < total; i++) {
-    randomCounts[i] = 0;
+    randomCounts[i] = 0; // initializing all indexes in radomCounts[20] to 0;
   }
 }
 
 function draw() {
-  background(127);
-  var index = floor(random(total));
+  background(127);//clears and re-draws canvas  background with grey color
+  var index = floor(random(total));//return a random number between 0 and total(exclusive)
   randomCounts[index]++;
 
   // Draw a rectangle to graph results
-  stroke(0);
-  strokeWeight(2);
-  fill(255);
+  stroke(0); //black stroke
+  strokeWeight(2); //stroke thickness of 2
+  fill(255); //fill rectangle with white
 
-  var w = width/randomCounts.length;
 
-  for (var x = 0; x < randomCounts.length; x++) {
+  var w = width/total; //calculating width of each rectangle/ histogram 
+
+  for (var x = 0; x < total; x++) {
     rect(x*w,height-randomCounts[x],w-1,randomCounts[x]);
   }
 }
