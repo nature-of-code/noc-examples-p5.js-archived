6d25d6afd83519786c76fe53ead775781afd329a
Mr-Darfler <33043742+Mr-Darfler@users.noreply.github.com>
2017-11-25 09:53:54 -0500

    Update sketch.js

--- chp00_introduction/NOC_I_04_Gaussian/sketch.js
+++ chp00_introduction/NOC_I_04_Gaussian/sketch.js
@@ -11,10 +11,10 @@ function setup() {
 function draw() {
 
   // Get a gaussian random number w/ mean of 0 and standard deviation of 1.0
-  var xloc = randomGaussian();
+  let xloc = randomGaussian();
 
-  var sd = 60;                // Define a standard deviation
-  var mean = width/2;         // Define a mean value (middle of the screen along the x-axis)
+  const sd = 60;                // Define a standard deviation
+  const mean = width/2;         // Define a mean value (middle of the screen along the x-axis)
   xloc = ( xloc * sd ) + mean;  // Scale the gaussian random number by standard deviation and mean
 
   fill(0, 10);
