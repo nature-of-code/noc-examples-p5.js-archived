8fb2d25c88c3b84d91bd86ce567508012607887e
Neil Ricci <nricci@plaid.com>
2020-03-20 13:26:15 -0700

    Complete Exercise 7 from Introduction

--- chp00_introduction/NOC_I_07_Noise2D/sketch.js
+++ chp00_introduction/NOC_I_07_Noise2D/sketch.js
@@ -1,5 +1,3 @@
-let increment = 0.02;
-
 function setup() {
     createCanvas(640, 360);
     noLoop();
@@ -8,23 +6,20 @@ function setup() {
 
 function draw() {
     background(0);
-
-    // Optional: adjust noise detail here
-    // noiseDetail(8,0.65f);
-
+    noiseDetail(8);
     loadPixels();
 
-    let xoff = 0.0;
-
+    let xoff = 0.0
     for (let x = 0; x < width; x++) {
         let yoff = 0.0;
         for (let y = 0; y < height; y++) {
             let bright = map(noise(xoff, yoff), 0, 1, 0, 255);
             let index = (x + y * width) * 4;
+            
             pixels[index ] = bright;
             pixels[index + 1] = bright;
             pixels[index + 2] = bright;
-            yoff += 0.01;
+            yoff += 0.01
         }
         xoff += 0.01;
     }
