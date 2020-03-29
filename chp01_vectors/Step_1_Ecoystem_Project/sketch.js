e10bb8bdd9b4dba25aa1e3b04c07c7f1feb35827
Neil Ricci <iccir919@gmail.com>
2020-03-28 22:24:24 -0700

    Finish a version of Ecoystem project

--- chp01_vectors/Step_1_Ecoystem_Project/sketch.js
+++ chp01_vectors/Step_1_Ecoystem_Project/sketch.js
@@ -1,45 +1,55 @@
 let creatures = [];
 
-let fly;
-let flyImg;
-let turtle;
-let turtleImg
-let snake;
-let snakeImg;
-
-
+let jellyfishImg;
+let goldfishImg;
+let sealImg;
+let crabImg;
 
 function preload() {
-    flyImg = loadImage('assets/fly.png');
-    turtleImg = loadImage('assets/turtle.png');
-    snakeImg = loadImage('assets/snake.png');
+    jellyfishImg = loadImage("assets/jellyfish.png");
+    goldfishImg = loadImage("assets/goldfish.png");
+    sealImg = loadImage("assets/seal.png");
+    crabImg = loadImage("assets/crab.png");
 }
 
 function setup() {
-    createCanvas(640, 340);
-    fly = new Fly();
-    turtle = new Turtle();
-    snake = new Snake();
-    creatures.push(fly, turtle, snake);
+    createCanvas(1000, 640);
+
+    let jellyfish = Array(7);
+    for (let i = 0; i < jellyfish.length; i++) {
+        jellyfish[i] = new Jellyfish(); 
+    }
+
+    let goldfish = Array(15);
+    for (let i = 0; i < goldfish.length; i++) {
+        goldfish[i] = new Goldfish(); 
+    }
+
+    let seal = [new Seal()]
+
+    let crab = Array(5);
+    for (let i = 0; i < crab.length; i++) {
+        crab[i] = new Crab(); 
+    }
+
+    creatures.push(jellyfish, goldfish, seal, crab);
 }
 
 function draw() {
     background(255);
-    for (let i = 0; i < creatures.length; i++) {
-        creatures[i].update();
-        creatures[i].display();
-        creatures[i].limit();
-    }
+    creatures.forEach((population) => {
+        population.forEach((creature) => {
+            creature.update();
+            creature.limit();
+            creature.display();
+        });
+    });
 }
 
 class Creature {
-    constructor() {
-        this.position = createVector(random(width), random(height));
-    }
-
     limit() {
-        this.position.x = constrain(this.position.x, 0, width-1);
-        this.position.y = constrain(this.position.y, 0, height-1);
+        this.position.x = constrain(this.position.x, 0, width-15);
+        this.position.y = constrain(this.position.y, 0, height-15);
     }
 
     display() {
@@ -48,85 +58,86 @@ class Creature {
 }
 
 
-class Turtle extends Creature {
+class Jellyfish extends Creature {
     constructor() {
         super();
-        this.img = turtleImg;
-        this.imgWidth = 100;
+        this.img = jellyfishImg;
+        this.imgWidth = 75;
         this.imgHeight = 100;
+        this.position = createVector(random(width), random(height / 2));
     }
 
     update() {
         const ran = random(1);
-        if (ran < 0.35) {
+        if (ran < 0.15) {
             let velocity = createVector(random(-2,2),random(-2,2));
             this.position.add(velocity);
         }
     }
-
-    acceptReject() {
-        let total = 10;
-        while (true) {
-            let r1 = floor(random(total));
-            let probability = r1;
-            let r2 = floor(random(total));
-    
-            if (r2 > probability) {
-                return r1;
-            }
-        }
-    }
-
-
 }
 
-class Fly extends Creature {
+class Goldfish extends Creature {
     constructor() {
         super();
-        this.img = flyImg;
-        this.imgWidth = 25;
-        this.imgHeight = 25;
+        this.img = goldfishImg;
+        this.imgWidth = 45;
+        this.imgHeight = 30;
+        this.noff = createVector(random(1000), random(1000));
+        this.position = createVector(width / 2, height / 2);
     }
 
     update() {
-        let velocity = createVector(random(-1,1), random(-1,1));
-        velocity.mult(montecarlo()*50);
-        this.position.add(velocity)
+        this.position.x = map(noise(this.noff.x),0,1,0,width);
+        this.position.y = map(noise(this.noff.y),0,1,0,height);
+        
+        this.noff.x += 0.01;
+        this.noff.y += 0.01;
     }
 }
 
-function montecarlo() {
-    while (true) {	
-  
-      let r1 = random(1);	
-      let probability = pow(1.0 - r1,8);	
-  
-      let r2 = random(1);	
-      if (r2 < probability) {	
-        return r1;
-      }
-    }
-}
-
-class Snake extends Creature {
+class Crab extends Creature {
     constructor() {
         super();
-        this.img = snakeImg;
+        this.img = crabImg;
+        this.imgWidth = 75;
+        this.imgHeight = 75;
+        this.position = createVector(random(width),height - 75);
+    }
+
+    update() {
+        const choice = random();
+        const stepSize = randomGaussian(5);
+
+        if (choice > 0.8) {    
+            this.position.x += stepSize;
+        } else if (choice < 0.2) {
+            this.position.x -= stepSize;
+        } 
+    }
+}
+
+class Seal extends Creature {
+    constructor() {
+        super();
+        this.img = sealImg;
         this.imgWidth = 130;
-        this.imgHeight = 90;
-
-        this.tx = 0;
-        this.ty = 10000;
+        this.imgHeight = 100;
+        this.position = createVector(width/2,height/2);
+        this.velocity = createVector();
+        this.acceleration = createVector();
+        this.topspeed = 5;
     }
 
     update() {
-        let velocity = createVector(
-            map(noise(this.tx), 0, 1, -8, 8),
-            map(noise(this.ty), 0, 1, -8, 8)
-        );
-        this.position.add(velocity)
-
-        this.tx += 0.007;
-        this.ty += 0.007;
+        let mouse = createVector(mouseX,mouseY);
+        this.acceleration = p5.Vector.sub(mouse,this.position);
+        // Set magnitude of acceleration
+        this.acceleration.setMag(0.2);
+    
+        this.velocity.add(this.acceleration);
+        this.velocity.limit(this.topspeed);
+        this.position.add(this.velocity);
     }
 }
+
+
